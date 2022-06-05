fb7eb802f0bebbe0b9dd49b61b7dd3eeb6e5e421
Qasim Mohammad Ali <90008402+qasdesign@users.noreply.github.com>
2022-06-05 22:28:29 +0300

    Create Contains_Duplicate.py

---
+++ Array_&_Hashing/Contains_Duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
